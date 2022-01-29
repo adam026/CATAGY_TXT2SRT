799cc38762b61d3b4fc7e60d8f81bb381c0a9a06
adam026 <76121613+adam026@users.noreply.github.com>
2022-01-29 19:37:50 +0100

    Add project files.

---
+++ CATAGY_TXT2SRT/IdozitettFelirat.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CATAGY_TXT2SRT
+{
+    class IdozitettFelirat
+    {
+        public string Idozites { get; set; }
+        public string Felirat { get; set; }
+        public IdozitettFelirat(KeyValuePair<string,string> adat)
+        {
+            Idozites = adat.Key;
+            Felirat = adat.Value;
+        }
+        public int SzavakSzama => Felirat.Split(' ').Count();
+        public string SrtIdozites => Kiszamol(Idozites);
+
+        private string Kiszamol(string felirat)
+        {
+            var buff = felirat.Split('-');
+            var Tol = buff[0].Trim();
+            var ora = 0;
+            var perc = int.Parse(Tol.Split(':')[0]);
+            var mp = int.Parse(Tol.Split(':')[1]);
+            if (perc > 60)
+            {
+                ora += 1;
+                perc = perc - 60;
+            }
+            var kezdoIp = new TimeSpan(ora, perc, mp);
+
+            var Ig = buff[1].Trim();
+
+            var oraIg = 0;
+            var percIg = int.Parse(Ig.Split(':')[0]);
+            var mpIg = int.Parse(Ig.Split(':')[1]);
+            if (percIg > 60)
+            {
+                oraIg += 1;
+                percIg = percIg - 60;
+            }
+            var vegIp = new TimeSpan(oraIg, percIg, mpIg);
+
+            return (kezdoIp + "-->" + vegIp);
+
+        }
+    }
+}
---
+++ CATAGY_TXT2SRT/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CATAGY_TXT2SRT
+{
+    internal class Program
+    {
+        static List<IdozitettFelirat> feliratok = new List<IdozitettFelirat>();
+        static Dictionary<string,string> adatok = new Dictionary<string,string>(); 
+        static void Main(string[] args)
+        {
+            Beolvas();
+            F05();
+            F07();
+            Kiir();
+        }
+
+        private static void Kiir()
+        {
+            var sw = new StreamWriter(@"..\..\RES\felirat.srt", false, Encoding.UTF8);
+            var counter = 1;
+            foreach (var felirat in feliratok)
+            {
+                sw.WriteLine($"{counter}\n{felirat.SrtIdozites}\n{felirat.Felirat}");
+                sw.WriteLine();
+                counter+=1;
+            }
+            Console.WriteLine("8. Feladat: Kiírás kész!");
+
+        }
+
+        private static void F07()
+        {
+            var lszaf = feliratok.OrderBy(x => x.SzavakSzama).Last();
+            Console.WriteLine($"7. Feladat - Legtöbb szóból álló felirat: \n{lszaf.Felirat}");
+        }
+
+        private static void F05()
+        {
+            Console.WriteLine($"5. Feladat - Feliratok száma: {adatok.Count}");
+        }
+
+        private static void Beolvas()
+        {
+            using (var sr = new StreamReader(@"..\..\RES\feliratok.txt", Encoding.UTF8))
+            {
+                while (!sr.EndOfStream)
+                {
+                    adatok.Add(sr.ReadLine(), sr.ReadLine());
+                }
+            }
+            foreach (var adat in adatok)
+            {
+                feliratok.Add(new IdozitettFelirat(adat));
+            }
+
+        }
+    }
+}
